dcd2675a8d85223fdb37f1970928e86450152ddc
Soham Anand Ladgaonkar <sohamladgaonkar19@gmail.com>
2024-12-04 12:49:21 +0530

    Inital message

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,18 +3,18 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">LUNA DINE</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p>D.N.Nagar, Mumbai</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By SOHAM LADGAONKAR</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By SOHAM LADGAONKAR.</p>
           </div>
         </div>
       </div>
