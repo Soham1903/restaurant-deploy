b6cc515df07adf4c8ae71cd46c39cb543c2f3e3d
Soham Anand Ladgaonkar <95856008+Soham1903@users.noreply.github.com>
2024-12-04 13:51:23 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://restaurant-website-uc78.onrender.com/api/v1/reservation/send",
+        "https://restaurant-deploy.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
